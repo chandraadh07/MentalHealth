c5ddd0a08a179b2d545faf3ceea4681c4118691e
Avahayako <avahayako>
2020-11-23 12:50:21 -0500

    test

---
+++ app/src/main/java/com/example/mentalhealth/LifeCycleInterface.kt
@@ -0,0 +1,8 @@
+package com.example.mentalhealth
+
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
+
+
+interface AddLifecycleCallbackListener {
+    fun addLifeCycleCallBack(youTubePlayerView: YouTubePlayerView?)
+}
